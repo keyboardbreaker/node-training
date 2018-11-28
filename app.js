718e5a50de938ab621e799ea93c30ae79fc5a9e6
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-11-28 01:44:51 +0000

    demonstrating first class function behaviour

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-a = 2;
-b = 3;
-console.log(a+b);
+// function statement
+function greet() {
+    console.log('hi');
+}
+
+// functions are first-class, meaning 
+// everything you can do with other types you can
+// can do with functions.
+// ie: pass them around, set them to variables
+function logGreeting(fn) {
+    fn();
+}
+
+greet();
+logGreeting(greet); //greet function being passed through to logGreeting
