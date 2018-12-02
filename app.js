5ec57a27403b32fc8b6ff28c1dd31dfd3edb5553
Yu-Jin Tan <yujin_1991@hotmail.com>
2018-12-02 16:19:40 +0000

    adding my first module

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-// function statement
-function greet() {
-    console.log('hi');
-}
-
-// functions are first-class, meaning 
-// everything you can do with other types you can
-// can do with functions.
-// ie: pass them around, set them to variables
-function logGreeting(fn) {
-    fn();
-}
-
-greet();
-logGreeting(greet); //greet function being passed through to logGreeting
+require('./greet.js');
