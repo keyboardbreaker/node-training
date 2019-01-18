f623e4e265cc140da4164008a6a952f100480952
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-18 14:36:38 +0000

    exploring the prototype chain with new function

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ Person.prototype.greet = function() {
     console.log(`Hello, ${this.firstName} ${this.lastName}`);
 }
 
+Person.prototype.salut = function() {
+    console.log(`I salut you`);
+}
+
 var john = new Person('John', 'Doe');
 john.greet();
 
 var jane = new Person('Jane', 'Smith');
 jane.greet();
 
+let phoebe = new Person('Phoebe', 'Josie');
+phoebe.salut();
+
 //look at the prototype method
-console.log(john.__proto__);
-console.log(jane.__proto__);
+// console.log(john.__proto__);
+// console.log(jane.__proto__);
 //are they the same prototype object
-console.log(john.__proto__ === jane.__proto__);
+// console.log(john.__proto__ === jane.__proto__);
