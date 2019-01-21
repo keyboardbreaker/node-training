89c4fd1e92859c2ca8e26990e28f9f817136a9fc
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-21 14:08:51 +0000

    adding multiple requires from a module folder

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-}
+var greet = require('./greet');
 
-Person.prototype.greet = function() {
-    console.log(`Hello, ${this.firstName} ${this.lastName}`);
-}
-
-Person.prototype.salut = function() {
-    console.log(`I salut you`);
-}
-
-var john = new Person('John', 'Doe');
-john.greet();
-
-var jane = new Person('Jane', 'Smith');
-jane.greet();
-
-let phoebe = new Person('Phoebe', 'Josie');
-phoebe.salut();
-
-//look at the prototype method
-// console.log(john.__proto__);
-// console.log(jane.__proto__);
-//are they the same prototype object
-// console.log(john.__proto__ === jane.__proto__);
+greet.english();
+greet.spanish();
